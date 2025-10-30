e4a5d621d5263786ae5e79fa3831c28ddf3c66ff
Abderrahmane AHMED <abderrahmane.ahmed@enpc.fr>
2025-10-30 11:57:08 +0100

    Modified Home page UI CSS and Add Application Controller , View for main menu CSS ; Setup structure View for add, update, delete, and list operations

---
+++ BankSystem/Controllers/ApplicationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankSystem.Controllers
+{
+    public class ApplicationController : Controller
+    {
+
+        public IActionResult index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
